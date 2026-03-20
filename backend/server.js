a2f98221c90c562239528d71c349d9202a863dc1
João Rangel <joaorangel.oficial@gmail.com>
2026-03-20 02:52:14 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -88,6 +88,7 @@ app.get("/consultas", autenticar, (req, res) => {
   res.json(consultas);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
 });
